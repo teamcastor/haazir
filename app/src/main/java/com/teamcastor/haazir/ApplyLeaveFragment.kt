8a41e31ce8069b51923be1c3c5c834f8c6cb6471
HemantVirk <77543285+HemantVirk@users.noreply.github.com>
2022-08-06 07:33:46 +0530

    Apply Leave Fragment

--- app/src/main/java/com/teamcastor/haazir/ApplyLeaveFragment.kt
+++ app/src/main/java/com/teamcastor/haazir/ApplyLeaveFragment.kt
@@ -2,73 +2,116 @@ package com.teamcastor.haazir
 
 import android.os.Build
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.teamcastor.haazir.databinding.FragmentApplyLeaveBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ApplyLeaveFragment : Fragment() {
 
-    var ch1: CheckBox? = null
-    var ch2: CheckBox? = null
-    var numofdays: TextView? = null
-    var numedit: EditText? = null
+    var half: RadioButton? = null
+    var regular: RadioButton? = null
+    var numofdays: LinearLayout? = null
+    var numofdaysedit: EditText? = null
+    var fromdate: TextView? = null
+    var from: LinearLayout? = null
+    var fromtext: TextView? = null
+    var till: LinearLayout? = null
+    var tilldate: TextView? = null
+    private var _binding: FragmentApplyLeaveBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?
     ): View? {
-        ch1 = view?.findViewById(R.id.half_day)
-        ch2 = view?.findViewById(R.id.regular)
-        numofdays = view?.findViewById(R.id.numofdays)
-        numedit = view?.findViewById(R.id.numedittext)
-        ch1?.setOnClickListener() { view ->
-            ch2!!.isChecked = false
-            System.out.println("half")
+        val materialDateBuilder: MaterialDatePicker.Builder<*> =
+            MaterialDatePicker.Builder.datePicker()
+        materialDateBuilder.setTitleText("SELECT A DATE")
+        val materialDatePicker = materialDateBuilder.build()
+        val materialDatePicker1 = materialDateBuilder.build()
+
+        _binding = FragmentApplyLeaveBinding.inflate(inflater,container,false)
+        from = binding.fromdate
+        fromdate = binding.fromdatetext
+        till = binding.tilldate
+        tilldate = binding.tilldatetext
+        numofdays = binding.numofdays
+        numofdaysedit = binding.numofdaysedit
+        fromtext = binding.from
+
+        from!!.setOnClickListener{
+            materialDatePicker.show(parentFragmentManager, "MATERIAL_DATE_PICKER")
         }
-        ch2?.setOnClickListener(){ view ->
-            ch1!!.isChecked = false
-            System.out.println("reg")
+        materialDatePicker.addOnPositiveButtonClickListener {
+            fromdate?.text = " " + materialDatePicker.headerText
         }
-//        fun itemClicked(view: View): Void?{
-//            if(ch1?.isChecked == true){
-//                numofdays?.keyListener=null
-//                numedit?.keyListener=null
-//            }
-//        }
-//        public void itemClicked(View v) {
-//            //code to check if this checkbox is checked!
-//            CheckBox checkBox = (CheckBox)v;
-//            if(checkBox.isChecked()){
-//
-//            }
-//        }
-//        if(ch1?.isChecked == true){
-//            numofdays?.keyListener=null
-//            numedit?.keyListener=null
-//        }
-        return inflater.inflate(R.layout.fragment_apply_leave, container, false)
+        till!!.setOnClickListener{
+            materialDatePicker1.show(parentFragmentManager, "MATERIAL_DATE_PICKER")
+        }
+        materialDatePicker1.addOnPositiveButtonClickListener {
+            tilldate?.text = " " + materialDatePicker1.headerText
+        }
+        numofdaysedit!!.filters = arrayOf<InputFilter>(MinMaxFilter(1, 7))
+
+//        numedit = binding.numedittext
+        half = binding.halfDay
+        regular = binding.regular
+
+        half!!.setOnClickListener{
+//            numofdays?.visibility = View.INVISIBLE
+//            numedit?.visibility = View.INVISIBLE
+            numofdays?.isEnabled = false
+            numofdaysedit?.isEnabled = false
+            tilldate?.isEnabled = false
+            fromtext!!.text = "Date"
+        }
+        regular!!.setOnClickListener(){
+            numofdays?.isEnabled = true
+            numofdaysedit?.isEnabled = true
+            tilldate?.isEnabled = true
+        }
+
+
+        return binding.root
     }
-//    fun half() {
-//        ch2!!.setChecked(false)
-//        System.out.println("half")
-////        ch2!!.jumpDrawablesToCurrentState()
-//    }
-////
-//    fun regular() {
-//        ch1!!.setChecked(false)
-//    System.out.println("reg")
-////        ch1!!.jumpDrawablesToCurrentState()
-//    }
+    inner class MinMaxFilter() : InputFilter {
+        private var intMin: Int = 0
+        private var intMax: Int = 0
+
+        // Initialized
+        constructor(minValue: Int, maxValue: Int) : this() {
+            this.intMin = minValue
+            this.intMax = maxValue
+        }
+
+        override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dStart: Int, dEnd: Int): CharSequence? {
+            try {
+                val input = Integer.parseInt(dest.toString() + source.toString())
+                if (isInRange(intMin, intMax, input)) {
+                    return null
+                }
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+            return ""
+        }
+        private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+            return if (b > a) c in a..b else c in b..a
+        }
+    }
+
 }
